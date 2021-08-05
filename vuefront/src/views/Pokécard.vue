8a262f07c0f43392d495a02978c0e183afc932da
liangyu cui <liangyu.cui@epitech.eu>
2021-08-05 12:04:05 +0200

    addfront

---
+++ vuefront/src/views/Pokécard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pokécard">
+    <h1>pokécard</h1>
+  </div>
+</template>
+<style>
+body {
+  background-image: url("../assets/jigglypuffbackground.jpg") !important;
+  background-repeat: no-repeat;
+   /* background-size: 1520px 155367px; */
+   background-size: cover;
+}
+</style>
---
+++ vuefront/src/views/Pokédex.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="pokédex">
+    <h1>pokédex</h1>
+  </div>
+</template>
